55ccbfc64a493f2d2981fe29735c4eaf8ce16290
Almaz K <34670358+Mazzz-zzz@users.noreply.github.com>
2025-12-13 19:30:48 +1100

    Initial Commit

---
+++ meta-stickersTests/meta_stickersTests.swift
@@ -0,0 +1,17 @@
+//
+//  meta_stickersTests.swift
+//  meta-stickersTests
+//
+//  Created by Almaz Khalilov on 13/12/2025.
+//
+
+import Testing
+@testable import meta_stickers
+
+struct meta_stickersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
